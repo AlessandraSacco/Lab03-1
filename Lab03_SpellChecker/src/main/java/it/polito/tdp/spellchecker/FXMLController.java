767915b60f0ee9955774cfa8b4dcf2f32b2f9988
Alessandra <Alessandra@LAPTOP-DT917UDD.fritz.box>
2021-03-28 12:28:38 +0200

    Modifiche1

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,77 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	Dictionary model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ComboBox<String> comb;
 
+    @FXML
+    private TextArea txtResult1;
+
+    @FXML
+    private Button btnCheck;
+
+    @FXML
+    private TextArea txtResult2;
+
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    void doButtonCheck(ActionEvent event) {
+    String parola= txtResult1.getText().toLowerCase();
+    parola.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]", "");
+    
+    
+    
+    	
+    	
+    	
     }
-}
 
+    @FXML
+    void doClearText(ActionEvent event) {
+     txtResult1.clear();
+     txtResult2.clear();
+    }
+
+    @FXML
+    void initialize() {
+        assert comb != null : "fx:id=\"cmbCombo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult1 != null : "fx:id=\"txtResult1\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult2 != null : "fx:id=\"txtResult2\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+  
+   
+    }
+
+	public void setModel(Dictionary model) {
+		this.model=model;
+		comb.getItems().addAll("English","Italian");
+		
+	}
+    
+    
+    
+}
 
